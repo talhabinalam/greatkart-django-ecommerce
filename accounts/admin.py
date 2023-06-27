37faf185731233c711af734379d972a1bcd59d53
talhabinalam <talhabinalam98@gmail.com>
2023-06-28 00:23:24 +0600

    initail commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,9 +4,8 @@ from .models import Account
 
 class AccountAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_staff', 'is_active')
-
+    readonly_fields = ('last_login', 'date_joined')
     ordering = ('-date_joined',)
-    
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
