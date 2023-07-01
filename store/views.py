d98b36a0045ed5873aa8112ec8eda79af86b0649
talhabinalam <talhabinalam98@gmail.com>
2023-07-01 14:27:02 +0600

    initail commit

--- store/views.py
+++ store/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+def store(request, category_slug=None):
+    categories = None
+    products = None
+    if category_slug != None:
+        categories = get_object_or_404(Category, category_slug=slug)
+        products=Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
     context = {
         'products': products,
         'product_count': product_count
