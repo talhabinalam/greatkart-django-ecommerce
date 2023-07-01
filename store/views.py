3aaa381810316da8e9bff989d2c6f0ffa704407a
talhabinalam <talhabinalam98@gmail.com>
2023-07-01 17:49:20 +0600

    initial commit

--- store/views.py
+++ store/views.py
@@ -6,7 +6,7 @@ def store(request, category_slug=None):
     categories = None
     products = None
     if category_slug != None:
-        categories = get_object_or_404(Category, category_slug=slug)
+        categories = get_object_or_404(Category, slug=category_slug)
         products=Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
     else:
@@ -20,4 +20,12 @@ def store(request, category_slug=None):
     return render(request, 'store/store.html', context)
 
 
-    
+def product_detail(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    context = {
+        'product': product
+    }
+    return render(request, 'store/product_detail.html', context)
