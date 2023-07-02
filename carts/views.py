4a43b07f64eff69e0c63d6a6205fd14a482b3eae
talhabinalam <talhabinalam98@gmail.com>
2023-07-02 14:59:18 +0600

    initial commit

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from store.models import Product
 from carts.models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -31,5 +32,20 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+    
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+    }
+        
+    return render(request, 'store/cart.html', context)
