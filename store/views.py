9cf03ade4fb3d818f81830975bf9ac1b27046437
talhabinalam <talhabinalam98@gmail.com>
2023-07-03 00:52:12 +0600

    initial commit

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
 
 def store(request, category_slug=None):
     categories = None
@@ -22,9 +24,12 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=product).exists()
+        
     except Exception as e:
         raise e
     context = {
-        'product': product
+        'product': product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
