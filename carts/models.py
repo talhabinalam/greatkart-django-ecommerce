c6f9f70977ceaf44efae35e57ec5aab98310e6ed
talhabinalam <talhabinalam98@gmail.com>
2023-07-02 20:48:16 +0600

    initial commit

--- carts/models.py
+++ carts/models.py
@@ -19,4 +19,4 @@ class CartItem(models.Model):
         return self.product.price * self.quantity
     
     def __str__(self):
-        return str(self.product.product_name)
+        return self.product.product_name
