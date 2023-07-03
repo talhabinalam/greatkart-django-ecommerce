f8f47071c3e3b5a77d157b2b85246ff3f2081609
talhabinalam <talhabinalam98@gmail.com>
2023-07-03 14:04:08 +0600

    initial commit

--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def store(request, category_slug=None):
     categories = None
@@ -16,7 +17,7 @@ def store(request, category_slug=None):
         paged_product = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.filter(is_available=True)
+        products = Product.objects.filter(is_available=True).order_by('id')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
@@ -49,3 +50,18 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    products = []
+    product_count = 0
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
