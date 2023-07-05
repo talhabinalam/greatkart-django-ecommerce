ebf242a1806c07687fff5b4bcc12dd25a81a6f30
talhabinalam <talhabinalam98@gmail.com>
2023-07-05 23:12:54 +0600

    working with authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,21 @@ from django import forms
 from .models import Account
 
 class RegistrationFrom(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Enter Password',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Confirm Password',
+    }))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+        
+    def __init__(self, *args, **kwargs):
+        super(RegistrationFrom, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
